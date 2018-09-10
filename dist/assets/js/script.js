e010bfde3364ad5c9d646481f82e304689214d9f
iskybow <kjionmd@gmail.com>
2018-09-10 14:45:05 +0300

    edits

--- dist/assets/js/script.js
+++ dist/assets/js/script.js
@@ -31,23 +31,6 @@ $(".scroll").on("click", function (e) {
   e.preventDefault();
 });
 
-if ($('.js_phone-mask').length > 0) {
-  $('.js_phone-mask').inputmask({ alias: "phoneru" });
-}
-
-// $(document).on('click', '.js_modalWindow', function () {
-//   $('body').css({'overflow': 'hidden'});
-//   $('.modal-window').addClass('modal-window-active');
-// });
-// $(document).on('click', '.overlay', function () {
-//   $('body').css({'overflow': 'visible'});
-//   $('.modal-window').removeClass('modal-window-active');
-// });
-
-$('.js_modalWindow').fancybox({
-  // Options will go here
-});
-
 $(document).on('click', '.js_listTwo', function () {
   $('.list-reviews').removeClass('js_listTwo');
   $('.list-reviews').addClass('js_listThree');
@@ -67,6 +50,23 @@ $(document).on('click', '.js_listFirst', function () {
   $('.list-reviews').addClass('js_listTwo');
   $('.reviews-list-three').fadeOut(0);
   $('.reviews-list-first').fadeIn();
+});
+
+if ($('.js_phone-mask').length > 0) {
+  $('.js_phone-mask').inputmask({ alias: "phoneru" });
+}
+
+// $(document).on('click', '.js_modalWindow', function () {
+//   $('body').css({'overflow': 'hidden'});
+//   $('.modal-window').addClass('modal-window-active');
+// });
+// $(document).on('click', '.overlay', function () {
+//   $('body').css({'overflow': 'visible'});
+//   $('.modal-window').removeClass('modal-window-active');
+// });
+
+$('.js_modalWindow').fancybox({
+  // Options will go here
 });
 
 // $(document).on('click', '.js_moreActive', function () {
